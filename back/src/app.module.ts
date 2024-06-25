c3e065de215028142eb887a57799f8722be69d43
PabloRodriguez16 <pablorodriguez6002@gmail.com>
2024-06-25 16:43:58 -0300

    conecté la BDD y completé el scaffolding

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,20 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import typeormConfig from './config/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [typeormConfig] }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        configService.get('typeorm'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ back/src/config/typeorm.ts
@@ -0,0 +1,26 @@
+/* eslint-disable prettier/prettier */
+import { config as dotenvConfig } from 'dotenv';
+import { registerAs } from '@nestjs/config';
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+dotenvConfig({ path: '.dev.env' });
+
+const config = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  migrations: ['dist/src/migrations/*{.ts,.js}'],
+  migrationsTableName: 'migrations',
+  autoloadEntities: true,
+  logging: ['error'],
+  migrationsRun: true,
+  synchronize: true,
+  // dropSchema: true,
+};
+export default registerAs('typeorm', () => config);
+export const connectDataSource = () =>
+  new DataSource(config as DataSourceOptions);
