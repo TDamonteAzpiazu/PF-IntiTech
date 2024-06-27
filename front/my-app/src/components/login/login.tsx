78b6520f700cd43065b2d4c13a67563d66071e89
SantiBerri <santiberri15@gmail.com>
2024-06-27 15:53:37 -0300

    login y form validation

--- front/my-app/src/components/login/login.tsx
+++ front/my-app/src/components/login/login.tsx
@@ -1,59 +1,200 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react';
+import styles from './style.module.css';
+import { validateLogin, validateRegister } from '@/helpers/formValidation';
+import { LoginErrorProps, RegisterErrorProps, Isession_active } from '@/interfaces/interfaces';
+import { login_auth } from '@/helpers/auth.login';
+import { useRouter } from 'next/navigation';
+import logo from '@/../../public/images/logonegro.png'
+import Image from 'next/image';
 
-const Login = () => {
+const AuthForm = () => {
+  const router = useRouter();
+  const [active, setActive] = useState(false);
+  const [userData, setUserData] = useState<Isession_active>();
+
+
+  useEffect(() => {
+    if (typeof window !== 'undefined' && window.localStorage) {
+      const userData: Isession_active = JSON.parse(localStorage.getItem('UserSession')!)
+      setUserData(userData)
+      userData?.token && alert('You are already logged in')
+      userData?.token && router.push('/') 
+  }
+
+  }, [])
+
+  //* Logica del registro
+
+
+  const [dataRegister, setdataRegister] = useState({
+    name: '',
+    email: '',
+    password: '',
+    address: '',
+    phone: '',
+    country: '',
+  });
+
+  const [errorRegister, setErrorRegister] = useState<RegisterErrorProps>({
+    name: '',
+    email: '',
+    password: '',
+    address: '',
+    phone: '',
+  });
+  const toggleClass = () => {
+    setActive(!active);
+  };
+
+  const handleChangeRegister = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target
+    setdataRegister({ ...dataRegister, [name]: value });
+    const errors = validateRegister(dataRegister)
+    setErrorRegister(errors)
+  }
+  const handleSubmitRegister = async () => {
+    try {
+      const res = await login_auth(dataRegister)
+      console.log(res)
+      alert('Register successful');
+    } catch (error: any) {
+      throw new Error(error);
+    }
+
+  }
+
+  //* Logica del login
+
+  const [dataLogin, setDataLogin] = useState({
+    email: '',
+    password: '',
+  })
+  const [error, setError] = useState<LoginErrorProps>({
+    email: '',
+    password: '',
+  });
+
+  const handleChangeLogin = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target
+    setDataLogin({ ...dataLogin, [name]: value });
+    const error = validateLogin(dataLogin)
+    setError(error)
+  }
+  const handleSubmitLogin = async (event: React.ChangeEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    try {
+      const res = await login_auth(dataLogin);
+      const { token, user } = await res;
+      localStorage.setItem('UserSession', JSON.stringify({ token, userData: user }));
+      alert('Login successful');
+      router.push('/')
+    } catch (error: any) {
+      throw new Error(error);
+    }
+  }
   return (
-    <div className="max-w-md relative flex flex-col p-4 rounded-md text-black bg-white">
-      <div className="text-2xl font-bold mb-2 text-[#000000] text-center">
-        Bienvenido a <span className="text-lightorangeinti">Inti tech</span>
-      </div>
-      <div className="text-sm font-normal mb-4 text-center text-[#000000]">
-        Ingresa a tu cuenta
-      </div>
-      <form className="flex flex-col gap-3">
-        <div className="block relative">
-          <label
-            htmlFor="email"
-            className="block text-gray-600 cursor-text text-sm leading-[140%] font-normal mb-2"
-          >
-            Email
-          </label>
-          <input
-            placeholder="example@gmail.com"
-            //onChange={handleChange}
-            required
-            //value={dataUser.email}
-            name="email"
-            type="text"
-            id="email"
-            className="rounded border border-gray-200 text-sm w-full font-normal leading-[18px] text-black tracking-[0px] appearance-none block h-11 m-0 p-[11px] focus:ring-2 ring-offset-2 ring-gray-900 outline-0"/>
+    <div className={styles.padre}>
+      <div className={`${styles.container} ${active ? styles.active : ''}`} id="container">
+        <div className={`${styles['form-container']} ${styles['sign-up']}`}>
+          <form onSubmit={handleSubmitRegister}>
+            <Image src={logo} alt="logo" width={100} height={100} />
+            <div className={styles['social-icons']}>
+              <a href="#" className={styles.icon}><i className="fa-brands fa-google-plus-g"></i></a>
+              <a href="#" className={styles.icon}><i className="fa-brands fa-facebook-f"></i></a>
+              <a href="#" className={styles.icon}><i className="fa-brands fa-linkedin-in"></i></a>
+            </div>
+            <span>or use your email for registration</span>
+            {errorRegister.email && <q className={styles.error}>{errorRegister.email}</q>}
+            <input
+              id='email'
+              name='email'
+              value={dataRegister.email}
+              onChange={handleChangeRegister}
+              type='email'
+              placeholder='Email' />
+            {errorRegister.password && <q className={styles.error}>{errorRegister.password}</q>}
+            <input
+              id='password'
+              name='password'
+              value={dataRegister.password}
+              onChange={handleChangeRegister}
+              type='password'
+              placeholder='Password' />
+            {errorRegister.name && <q className={styles.error}>{errorRegister.name}</q>}
+            <input
+              id='name'
+              name='name'
+              value={dataRegister.name}
+              onChange={handleChangeRegister}
+              type='text'
+              placeholder='Name' />
+            {errorRegister.address && <q className={styles.error}>{errorRegister.address}</q>}
+            <input
+              id='address'
+              name='address'
+              value={dataRegister.address}
+              onChange={handleChangeRegister}
+              type='text'
+              placeholder='Address' />
+            {errorRegister.phone && <q className={styles.error}>{errorRegister.phone}</q>}
+            <input
+              id='phone'
+              name='phone'
+              value={dataRegister.phone}
+              onChange={handleChangeRegister}
+              type='tel'
+              placeholder='Phone' />
+            <button type='submit'>Sign Up</button>
+          </form>
         </div>
-        <div className="block relative">
-          <label
-            htmlFor="password"
-            className="block text-gray-600 cursor-text text-sm leading-[140%] font-normal mb-2"
-          >
-            Contraseñaa
-          </label>
-          <input
-            placeholder="**********"
-            //onChange={handleChange}
-            required
-            //value={dataUser.password}
-            name="password"
-            type="password"
-            id="password"
-            className="rounded border border-gray-200 text-sm w-full font-normal leading-[18px] text-black tracking-[0px] appearance-none block h-11 m-0 p-[11px] focus:ring-2 ring-offset-2 ring-gray-900 outline-0"
-          />
+        <div className={`${styles['form-container']} ${styles['sign-in']}`}>
+          <form onSubmit={handleSubmitLogin}>
+            <Image src={logo} alt="logo" width={100} height={100} />
+            <h1 className={styles.title}>Sign In</h1>
+            <div className={styles['social-icons']}>
+              <a href="#" className={styles.icon}><i className="fa-brands fa-google-plus-g"></i></a>
+              <a href="#" className={styles.icon}><i className="fa-brands fa-facebook-f"></i></a>
+              <a href="#" className={styles.icon}><i className="fa-brands fa-linkedin-in"></i></a>
+            </div>
+            <span>or use your email for password</span>
+            {error.email && <q className={styles.error}>{error.email}</q>}
+            <input
+              id='emailLogin'
+              name='email'
+              value={dataLogin.email}
+              onChange={handleChangeLogin}
+              type='email'
+              placeholder='Email' />
+            {error.password && <q className={styles.error}>{error.password}</q>}
+            <input
+              id='passwordLogin'
+              name='password'
+              value={dataLogin.password}
+              onChange={handleChangeLogin}
+              type='password'
+              placeholder='Password' />
+            <a href="#">Forget Your Password?</a>
+            <button type='submit'>Sign In</button>
+          </form>
         </div>
-        <button type="submit" className={`block hover:brightness-110 hover:animate-pulse font-bold py-3 px-6 rounded-full bg-lightorangeinti shadow-lg shadow-lightorangeinti text-black text-center cursor-pointer`}>
-          Iniciar Sesion
-        </button>
-      </form>
-      <div className="text-sm text-center mt-[1.6rem]">
-        No tienes una cuenta?{" "}
+        <div className={styles['toggle-container']}>
+          <div className={styles.toggle}>
+            <div className={`${styles['toggle-panel']} ${styles['toggle-left']}`}>
+              <h1 className={styles.welcome}>Welcome Back!</h1>
+              <p>Enter your personal details to use all of site features</p>
+              <button className={styles.hidden} onClick={toggleClass}>Sign In</button>
+            </div>
+            <div className={`${styles['toggle-panel']} ${styles['toggle-right']}`}>
+              <h1 className={styles.welcome} >Hello, Friend!</h1>
+              <p>Register with your personal details to use all of site features</p>
+              <button className={styles.hidden} onClick={toggleClass}>Sign Up</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default AuthForm;
