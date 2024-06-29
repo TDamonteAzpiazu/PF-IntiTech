91ecdb108e93eccc272649de3dac59c7f4792f22
benjadelcampo15 <benjadelcampo15@gmail.com>
2024-06-29 11:43:00 -0400

    class validators listos

--- back/src/entities/operatingPanels.entity.ts
+++ back/src/entities/operatingPanels.entity.ts
@@ -2,13 +2,17 @@ import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "ty
 import {v4 as uuid} from "uuid";
 import { Inversor } from "./inversor.entity";
 import { Stats } from "./stats.entity";
+import { IsNotEmpty, IsString, IsUUID } from "class-validator";
 @Entity({name : "panelstats"})
 export class OperatingPanels {
 
     @PrimaryGeneratedColumn("uuid")
+    @IsUUID()
     id: string = uuid;
 
     @OneToMany(() => Inversor, (inversor) => inversor.operatingPanels)
+    @IsString()
+    @IsNotEmpty()
     inversor : string;
 
     @OneToMany(() => Stats, stats => stats.operatingPanel)
