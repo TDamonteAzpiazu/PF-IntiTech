53aa44f88c789cbd3a046df7f558a7c3146945e4
Lucas S DE LA TORRE <lucasebasdlt@gmail.com>
2024-06-30 04:32:32 -0300

    se modifican archivos dentro de button_google

--- front/my-app/src/components/google_button/prueba.tsx
+++ front/my-app/src/components/google_button/prueba.tsx
@@ -1,56 +1,63 @@
+'use client'
+
 import React, { useEffect, useState } from "react";
-import { useRouter } from "next/router";
+import { useSearchParams } from "next/navigation";
 import { Idata_google, auth_google, post_auth } from "@/helpers/auth.google";
+import Button from "./index"; 
 import { jwtDecode } from "jwt-decode";
 
 export interface Itoken {
-  token: string;
+  token: string | null;
   name: string;
   email: string;
 }
 
-{/*export const Button: React.FC = () => {
-  const router = useRouter();
+const Google_Button: React.FC = () => {
+  const searchParams = useSearchParams();
   const [data, setData] = useState<any | null>(null);
 
   useEffect(() => {
     const get_data_google = async () => {
       try {
-        if (!router.isReady) return; // Verificar que el router esté listo
-        const { token } = router.query;
+        const token =  searchParams.get('token');
+        console.log("Token:", token);
+
         if (token) {
-          const decode_token: Itoken = jwtDecode(token as string);
+          const decode_token: Itoken  = jwtDecode(token);
+          console.log("Decoded Token:", decode_token); 
           const autentication_data: Idata_google = {
             email: decode_token.email,
             name: decode_token.name,
           };
           const data = await auth_google(autentication_data);
+          console.log("Auth Data:", data);
           setData(data);
         }
       } catch (error) {
-        console.error("error en funcion get_data_google:", error);
+        console.error("Error en función get_data_google:", error);
       }
     };
 
-    get_data_google(); // Llamar a la función dentro del useEffect
-
-  }, [router.isReady, router.query]);
+    get_data_google();
+  }, [searchParams]);
 
   const post_data_google = async () => {
     try {
       if (data) {
+        console.log("log prueba Data:", data); 
         await post_auth(data);
+        console.log("log de prueba Auth Data:", data); 
       }
     } catch (error) {
-      console.error("error en funcion post_data_google:", error);
+      console.error("Error en función post_data_google:", error);
     }
   };
 
   return (
     <div>
-      <button onClick={post_data_google}>BUTTON DE PRUEBA</button>
+      <Button onClick={post_data_google} />
     </div>
   );
 };
 
-export default Button;*/}
+export default Google_Button;
