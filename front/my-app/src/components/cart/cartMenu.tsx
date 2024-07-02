c3b6e7ae7ba9dce74c90c98ac5ad31e1026e8945
SantiBerri <santiberri15@gmail.com>
2024-07-02 17:45:01 -0300

    menu del carrito

--- front/my-app/src/components/cart/cartMenu.tsx
+++ front/my-app/src/components/cart/cartMenu.tsx
@@ -14,9 +14,7 @@ const Cart = () => {
           isOpen ? 'translate-x-0' : 'translate-x-full'
         }`}
       >
-        <button onClick={toggleCart} className="absolute top-4 right-4 z-50 text-black p-2 rounded-md">
-          Close
-        </button>
+
         <div className="p-4">
           <h2 className="text-2xl mb-4">Shopping Cart</h2>
           {/* nashe */}
