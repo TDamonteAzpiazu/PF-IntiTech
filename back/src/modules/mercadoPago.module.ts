e21bc1db3914d2601a33b22d209918ad12ab2139
TDamonteAzpiazu <124930230+TDamonteAzpiazu@users.noreply.github.com>
2024-07-02 12:48:36 -0300

    Revert "algo de mp"

--- back/src/modules/mercadoPago.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-
-
-@Module({
-  imports: [MercadoPagoModule],
-  controllers: [mercadoPagoController],
-  providers: [mercadoPagoService , mercadoPagoRepository],
-})
-export class AppModule {}
