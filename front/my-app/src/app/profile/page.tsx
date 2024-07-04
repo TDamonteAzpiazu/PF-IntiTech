eee4dd1f78f58e93c58aec53257f2a91953c8940
Bruno Distaulo <0800bruno03@gmail.com>
2024-07-04 12:04:48 -0300

    posibles errores

--- front/my-app/src/app/profile/page.tsx
+++ front/my-app/src/app/profile/page.tsx
@@ -2,16 +2,57 @@
 
 import { DataUser } from "@/interfaces/interfaces";
 import React, { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 const Profile = () => {
     const [userData, setUserData] = useState<DataUser | null>(null);
     const [inputs, setInputs] = useState({
         name: '',
         email: '',
-        password: '',
         address: '',
         phone: '',
     });
+    const [password, setPassword] = useState('');
+
+    const [selectedFile, setSelectedFile] = useState<File | null>(null);
+
+
+
+
+    const handle_FileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files ? e.target.files[0] : null;
+        setSelectedFile(file);
+        console.log(file);
+    };
+
+    const handleFileChange = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const formData = new FormData();
+        if (selectedFile) {
+            formData.append('file', selectedFile);
+        }
+        try {
+            const res = await fetch(`http://localhost:3000/files/uploadUserImage/${userData?.id}`, {
+                method: 'POST',
+                body: formData
+            });
+
+            if (!res.ok) {
+                console.error("Error en la carga");
+
+            }
+
+            const responseData = await res.json();
+            console.log(responseData);
+        } catch (error) {
+            console.error("Un error ocurrió:", error);
+        }
+    };
+
+
+    
+
+
 
     useEffect(() => {
         if (typeof window !== 'undefined' && window.localStorage) {
@@ -20,7 +61,6 @@ const Profile = () => {
             setInputs({
                 name: storedUserData?.name || '',
                 email: storedUserData?.email || '',
-                password: '',
                 address: storedUserData?.address || '',
                 phone: storedUserData?.phone || '',
             });
@@ -32,15 +72,23 @@ const Profile = () => {
         setInputs({ ...inputs, [name]: value });
     };
 
-    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
+    const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.target.value);
+    };
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const updatedData = {
+            ...inputs,
+            password: password || undefined, // Only include password if it has been changed
+        };
         try {
             const res = await fetch(`http://localhost:3000/users/${userData?.id}`, {
                 method: 'PUT',
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify(inputs),
+                body: JSON.stringify(updatedData),
             });
             if (res.ok) {
                 const updatedUser = await res.json();
@@ -49,11 +97,10 @@ const Profile = () => {
                 setInputs({
                     name: updatedUser.name,
                     email: updatedUser.email,
-                    password: '',
                     address: updatedUser.address,
                     phone: updatedUser.phone,
                 });
-                window.location.reload();
+                setPassword(''); // Clear password field after successful update
             } else {
                 console.log('Error:', res.statusText);
             }
@@ -63,8 +110,8 @@ const Profile = () => {
     };
 
     return (
-        <div className="pt-24">
-            <form className="mx-10" onSubmit={handleSubmit}>
+        <div className="pt-24 flex justify-center gap-12">
+            <form className=" border" onSubmit={handleSubmit}>
                 <h1 className="text-3xl text-white font-medium py-3">Configuración de perfil</h1>
                 <p className="text-white text-sm font-light pb-4">Puedes cambiar tus datos personales</p>
                 <div className="flex justify-around py-11">
@@ -80,7 +127,7 @@ const Profile = () => {
                         <label className="text-white text-lg">Correo:</label>
                         <input
                             name="email"
-                           
+
                             onChange={handleChange}
                             className="h-9 text-white bg-transparent border-b border-yellowinti p-2 mb-8 placeholder:p-2 placeholder:italic focus:outline-none"
                             placeholder={userData?.email || "Correo"}
@@ -89,7 +136,7 @@ const Profile = () => {
                         <label className="text-white text-lg">Contraseña:</label>
                         <input
                             name="password"
-                            
+
                             onChange={handleChange}
                             className="h-9 text-white bg-transparent border-b border-yellowinti p-2 mb-8 placeholder:p-2 placeholder:italic focus:outline-none"
                             placeholder="Contraseña"
@@ -100,7 +147,7 @@ const Profile = () => {
                         <label className="text-white text-lg">Dirección:</label>
                         <input
                             name="address"
-                            
+
                             onChange={handleChange}
                             className="h-9 text-white bg-transparent border-b border-yellowinti p-2 mb-8 placeholder:p-2 placeholder:italic focus:outline-none"
                             placeholder={userData?.address || "Dirección"}
@@ -109,20 +156,30 @@ const Profile = () => {
                         <label className="text-white text-lg">Teléfono:</label>
                         <input
                             name="phone"
-                            
+
                             onChange={handleChange}
                             className="h-9 text-white bg-transparent border-b border-yellowinti p-2 mb-8 placeholder:p-2 placeholder:italic focus:outline-none"
                             placeholder={userData?.phone || "Teléfono"}
                             type="text"
                         />
-                        <label className="text-white text-lg">Imagen:</label>
-                        <input
-                            className="h-9 p-2 mb-8 placeholder:p-2 placeholder:italic focus:outline-none text-white"
-                            type="file"
-                        />
+
                     </div>
                 </div>
                 <button type="submit" className="mx-auto flex my-10 w-fit h-fit px-4 py-3 rounded-xl bg-lightorangeinti text-white font-medium">Guardar cambios</button>
+            </form>
+            <form className="flex flex-col border w-96" onSubmit={handleFileChange}>
+                <div>
+                    <label className="text-white text-lg">Imagen actual:</label>
+                    <img src={userData?.image} alt="imagen" className="w-32 h-32" />
+                </div>
+
+                <label className="text-white text-lg">Cambiar imagen:</label>
+                <input
+                    onChange={handle_FileChange}
+                    className="h-9 p-2 mb-8 placeholder:p-2 placeholder:italic focus:outline-none text-white"
+                    type="file"
+                />
+                <button className="bg-lightorangeinti py-2 px-4 text-white font-medium rounded-xl" >Cargar imagen</button>
             </form>
         </div>
     );
