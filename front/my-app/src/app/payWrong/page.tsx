e9391fc62b33a64c7a440d2d00032e0fad64561e
SantiBerri <santiberri15@gmail.com>
2024-07-05 13:28:33 -0300

    pays

---
+++ front/my-app/src/app/payWrong/page.tsx
@@ -0,0 +1,13 @@
+import PayWrong from '@/components/payView_wrong'
+import Pay from '@/components/payView_wrong'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <PayWrong />
+    </div>
+  )
+}
+
+export default page
