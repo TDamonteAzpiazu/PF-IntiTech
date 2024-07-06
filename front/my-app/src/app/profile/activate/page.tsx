117aca18f0e13160e9fc3a0c37adde7a9916fe9f
Bruno Distaulo <0800bruno03@gmail.com>
2024-07-06 18:28:22 -0300

    activar cuenta hecho

---
+++ front/my-app/src/app/profile/activate/page.tsx
@@ -0,0 +1,67 @@
+'use client';
+import { DataUser } from "@/interfaces/interfaces";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+export interface User {
+    id: string;
+    name: string;
+    email: string;
+    password: string;
+    address: string;
+    phone: string;
+    image?: string;
+    status?: 'active' | 'inactive' | 'pending';
+}
+
+const Activate = () => {
+    const [newUser, setNewUser] = useState<User | null>(null);
+    const router = useRouter();
+
+    useEffect(() => {
+        if (typeof window !== 'undefined' && window.localStorage) {
+            const storedUserData: User = JSON.parse(localStorage.getItem('DataUser')!);
+            if (storedUserData) {
+                setNewUser(storedUserData);
+                if (storedUserData.status === 'active') {
+                    alert('Usuario ya activado');
+                    router.push('/profile');
+                }
+            }
+        }
+    }, [router]);
+
+    const handleClick = async () => {
+        if (!newUser) return;
+
+        const response = await fetch(`http://localhost:3000/users/${newUser.id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ status: "active" }),
+        });
+
+        const res = await response.json();
+        if (res) {
+            localStorage.setItem('DataUser', JSON.stringify(res));
+            if (res.status === 'active') {
+                alert('Usuario activado con éxito');
+                router.push('/profile');
+            }
+        }
+    };
+
+    return (
+        <div className="bg-black h-screen flex justify-center items-center">
+            <button 
+                onClick={handleClick}
+                className="h-fit text-white text-2xl font-medium py-3 px-6 border border-white rounded-xl hover:bg-yellowinti hover:text-black transition-all duration-500 ease-in-out"
+            >
+                Activa tu cuenta
+            </button>
+        </div>
+    );
+};
+
+export default Activate;
