35fa16041682048a90daf10162c9a6935d12059f
Bruno Distaulo <0800bruno03@gmail.com>
2024-07-07 22:00:59 -0300

    creo que esta roto auth repository

--- front/my-app/src/helpers/auth.login.ts
+++ front/my-app/src/helpers/auth.login.ts
@@ -1,51 +1,53 @@
-import { Ilogin_props, Iregister_props } from '@/interfaces/interfaces'
+import { Ilogin_props, Iregister_props } from "@/interfaces/interfaces";
 import Swal from "sweetalert2";
 
-const api_url = process.env.NEXT_PUBLIC_API_URL
+const api_url = process.env.NEXT_PUBLIC_API_URL;
 
 export async function login_auth(data_user: Ilogin_props) {
   try {
     const res = await fetch(`${api_url}/auth/login`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(data_user),
-    })
+    });
     if (res.ok) {
-      return res.json()
+      return res.json();
     } else {
-      alert('Failed to login')
-      throw new Error('Failed to login')
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Debes llenar todos los campos",
+      });
     }
-  } catch (error: any) {
-    throw new Error(error)
+  } catch (error) {
+    console.log(error)
   }
 }
-
 
 export async function register_auth(data_register: Iregister_props) {
   try {
     const res = await fetch(`${api_url}/auth/register`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-type': 'application/json',
+        "Content-type": "application/json",
       },
       body: JSON.stringify(data_register),
-    })
-    console.log(res)
+    });
+    console.log(res);
     if (res.ok) {
       Swal.fire({
-        icon: 'success',
-        title: 'Registered successfully',
+        icon: "success",
+        title: "Registered successfully",
         showConfirmButton: false,
-        timer: 2000
-      })
+        timer: 2000,
+      });
       return res.json();
     } else {
-      alert('Failed to register')
+      alert("Failed to register");
     }
   } catch (error: any) {
-    throw new Error(error.message || 'se produjo un error durante el registro')
+    throw new Error(error.message || "se produjo un error durante el registro");
   }
 }
