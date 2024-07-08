73e243905543ed08579b351e81b08e6237d3e0e2
Bruno Distaulo <0800bruno03@gmail.com>
2024-07-07 22:57:06 -0300

    probando deploy

--- front/my-app/src/app/dashboard/pie.js
+++ front/my-app/src/app/dashboard/pie.js
@@ -1,75 +1,75 @@
-import { Doughnut } from "react-chartjs-2";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-import { useEffect, useState } from "react";
+// import { Doughnut } from "react-chartjs-2";
+// import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+// import { useEffect, useState } from "react";
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+// ChartJS.register(ArcElement, Tooltip, Legend);
 
-export function Circular_Chart({ stats }) {
-  const [chartData, setChartData] = useState({
-    labels: ["opcion 1", "opcion 2"],
-    datasets: [
-      {
-        label: "Consumo total / Inyeccion total",
-        data: [10, 20, 30, 40],
-        backgroundColor: ["#e18104", "#f7a90e", "#ffed00"],
-        hoverBackgroundColor: ["#ffa500", "#ffd700"],
-        borderColor: ["grey"],
-        borderWidth: 1,
-      },
-    ],
-  });
+// export function Circular_Chart({ stats }) {
+//   const [chartData, setChartData] = useState({
+//     labels: ["opcion 1", "opcion 2"],
+//     datasets: [
+//       {
+//         label: "Consumo total / Inyeccion total",
+//         data: [10, 20, 30, 40],
+//         backgroundColor: ["#e18104", "#f7a90e", "#ffed00"],
+//         hoverBackgroundColor: ["#ffa500", "#ffd700"],
+//         borderColor: ["grey"],
+//         borderWidth: 1,
+//       },
+//     ],
+//   });
 
-  useEffect(() => {
+//   useEffect(() => {
    
-    setChartData({
-      labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-      datasets: [
-        {
-          label: "Generacion diaria ",
-          data: stats.map((item) => item), 
-          backgroundColor: ["#e18104", "#f7a90e", "#ffed00"], 
-          hoverBackgroundColor: ["#ffa500", "#ffd700"], 
-          borderColor: ["grey"],
-          borderWidth: 1,
-        },
-      ],
-    });
-  }, [stats]);
+//     setChartData({
+//       labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+//       datasets: [
+//         {
+//           label: "Generacion diaria ",
+//           data: stats.map((item) => item), 
+//           backgroundColor: ["#e18104", "#f7a90e", "#ffed00"], 
+//           hoverBackgroundColor: ["#ffa500", "#ffd700"], 
+//           borderColor: ["grey"],
+//           borderWidth: 1,
+//         },
+//       ],
+//     });
+//   }, [stats]);
 
-  const pie_options = {
-    responsive: true,
-    maintainAspectRatio: false,
-    animation: {
-      animateScale: true,
-      animateRotate: true,
-    },
-    plugins: {
-      legend: {
-        position: "top",
-        labels: {
-          font: {
-            size: 14,
-          },
-          color: "black",
-        },
-      },
-      tooltip: {
-        enabled: true,
-        backgroundColor: "rgba(0, 0, 0, 0.7)",
-        titleFont: {
-          size: 16,
-        },
-        bodyFont: {
-          size: 14,
-        },
-        callbacks: {
-          label: function (tooltipItem) {
-            return `Valor: ${tooltipItem.raw}`;
-          },
-        },
-      },
-    },
-  };
+//   const pie_options = {
+//     responsive: true,
+//     maintainAspectRatio: false,
+//     animation: {
+//       animateScale: true,
+//       animateRotate: true,
+//     },
+//     plugins: {
+//       legend: {
+//         position: "top",
+//         labels: {
+//           font: {
+//             size: 14,
+//           },
+//           color: "black",
+//         },
+//       },
+//       tooltip: {
+//         enabled: true,
+//         backgroundColor: "rgba(0, 0, 0, 0.7)",
+//         titleFont: {
+//           size: 16,
+//         },
+//         bodyFont: {
+//           size: 14,
+//         },
+//         callbacks: {
+//           label: function (tooltipItem) {
+//             return `Valor: ${tooltipItem.raw}`;
+//           },
+//         },
+//       },
+//     },
+//   };
 
-  return <Doughnut data={chartData} options={pie_options} />;
-}
+//   return <Doughnut data={chartData} options={pie_options} />;
+// }
