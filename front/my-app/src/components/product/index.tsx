36039de8098ad4aafda848b3047dd0fba46acbd6
SantiBerri <santiberri15@gmail.com>
2024-07-08 22:14:36 -0300

    productos arreglados

--- front/my-app/src/components/product/index.tsx
+++ front/my-app/src/components/product/index.tsx
@@ -14,34 +14,32 @@ const Product: React.FC<Iproducts_props> = ({
   image,
 }) => {
   return (
-    <div className="grid grid-cols-2 mt-2 transition-all hover:shadow-2xl rounded-lg justify-center gap-1 h-60 ">
-
-        <div className="relative sm:h-70 sm:w-60 ">
+    <div className="flex justify-center">
+      <div className="flex mt-2 transition-all hover:scale-105 rounded-lg h-60 w-9/12">
+        <div className="relative w-60 h-full">
           <Image
-            className="rounded-l-md"
+            className="rounded-l-md object-cover"
             src={image}
             alt="Product Image"
-            layout="fill"/>
+            layout="fill"
+          />
         </div>
-      
-
-        <div className="px-4 py-4 bg-gradient-to-r from-orange-400  to-orange-200 bg-transparent opacity-85 rounded-r-md ">
+        <div className="flex-1 px-4 py-4 bg-gradient-to-r from-white to-gray-100 rounded-r-md">
           <div className="flex items-center justify-between mb-2">
-            <h1 className="text-lg font-semibold text-gray-900 ">{brand}</h1>
-            <span className="text-gray-900  text-sm">Stock: {stock}</span>
+            <h1 className="text-lg font-semibold text-gray-900">{brand}</h1>
+            <span className="text-gray-900 text-sm">Stock: {stock}</span>
           </div>
 
           <h2 className="text-xl font-semibold text-gray-900 mb-2">{model}</h2>
 
-
-              <Link href={`product_detail/${id}`}>
-            <button className="bg-gradient-to-r from-yellow-300 to-orange-400 opacity-95 hover:shadow-xl text-gray-900  px-4 py-2 rounded-lg transition-all duration-300 hover:scale-105">
+          <Link href={`product_detail/${id}`}>
+            <button className="bg-gradient-to-r from-yellow-300 to-orange-400 opacity-95 hover:shadow-xl text-gray-900 px-4 py-2 rounded-lg transition-all duration-300 hover:scale-105">
               Ver detalles
             </button>
           </Link>
         </div>
+      </div>
     </div>
-    
   );
 };
 
