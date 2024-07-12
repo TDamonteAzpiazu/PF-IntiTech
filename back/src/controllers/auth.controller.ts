9af531d56eef81471d3734c812ed8e3f0fcb26c6
TDamonteAzpiazu <tobidamonte@gmail.com>
2024-07-12 12:51:52 -0300

    Cambio de redirecciones al nuevo deploy de front

--- back/src/controllers/auth.controller.ts
+++ back/src/controllers/auth.controller.ts
@@ -62,6 +62,6 @@ export class AuthController {
     if (isNew) {
       await this.authService.sendEmail(createdUser, jwt);
     }
-    res.status(HttpStatus.OK).redirect(`http://localhost:3001/auth/google?token=${jwt}`);
+    res.status(HttpStatus.OK).redirect(`http://pf-inti-tech-okrv.vercel.app/auth/google?token=${jwt}`);
   }
 }
